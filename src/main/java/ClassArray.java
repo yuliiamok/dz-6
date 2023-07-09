9a9411ab5d57bf2272157bb6b170c5569791976b
Yuliia Mokina <iuliia.mokina@gmail.com>
2023-07-09 16:26:12 +0200

    Changed dz-6 according to the homework task

--- src/main/java/ClassArray.java
+++ src/main/java/ClassArray.java
@@ -3,57 +3,49 @@ import java.util.stream.Collectors;
 
 public class ClassArray {
     public static void main(String[] args) {
-        String[] stringArray = {"мама", "папа", "їж їжак желе"};
 
-        System.out.println(Arrays.toString(stringArray));
-
-        for (int i = 0; i < stringArray.length; i++) {
-            System.out.println(stringArray[i]);
-
-            Map<Character, Integer> result = new HashMap<>();
-                for (char ch : stringArray[i].toCharArray()) {
-                    if (Character.isLetter(ch)) {
-                        int count = result.containsKey(ch) ? result.get(ch) : 0;
-                        result.put(ch, count + 1);
-                    }
-                }
-
-                for (Map.Entry<Character, Integer> pair : result.entrySet()) {
-
-                    if (pair.getValue() % 2 == 0) {
-                        System.out.println(pair.getKey() + " : " + pair.getValue());
-                    }
-                }
-
-        }
-        System.out.println("--------------------------------");
-
-        Set<Character> letters = new HashSet<>();
-        for (String words : stringArray) {
-            for (char ch : words.toCharArray()) {
-                letters.add(ch);
-            }
-        }
-
-        System.out.println("Список унікальних символів: ");
-        System.out.println(letters);
-
-
-        System.out.println("--------------------------------");
-        System.out.println("Набір всіх літер:");
-        for (String phrase : stringArray) {
-            char[] ch = phrase.toCharArray();
-
-            for (int i = 0; i < ch.length; i++) {
-                if (ch[i] == ' ')
-                    continue;
-                System.out.print(ch[i]+ ", ");
-
-            }
-        }
-
-
+        printEvenCharacters(new String[]{"мама", "папа", "їж їжак желе"});
     }
 
-
+    public static void printEvenCharacters(String[] phrases) {
+        List<String> evenPhrases = new ArrayList<>();
+        int counter = 0;
+        for (String phrase : phrases) {
+            Map<Character, Integer> charCount = countCharacters(phrase);
+            if (isEvenCountChars(charCount)) {
+                evenPhrases.add(phrase);
+                counter++;
+                if (counter == 2)
+                    break;
+            }
+        }
+        Set<Character> uniqueChars = getUniqueCharacters(evenPhrases);
+        System.out.println(uniqueChars);
+    }
+    private static Map<Character, Integer> countCharacters(String phrase) {
+        char[] chars = phrase.toCharArray();
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : chars) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
+        return charCount;
+    }
+    private static Set<Character> getUniqueCharacters(List<String> list) {
+        Set<Character> uniqueChars = new HashSet<>();
+        for (String phrase : list) {
+            char[] chars = phrase.toCharArray();
+            for (char ch : chars)
+                uniqueChars.add(ch);
+        }
+        return uniqueChars;
+    }
+    private static boolean isEvenCountChars(Map<Character, Integer> charCount) {
+        boolean isEven = false;
+        for (Character ch : charCount.keySet()) {
+            System.out.println(ch + " - " + charCount.get(ch));
+            isEven = charCount.get(ch) % 2 == 0;
+            if (!isEven) break;
+        }
+        return isEven;
+    }
 }
